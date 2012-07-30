2890496102ac9e3f4a325b138b6c72a6da975b89
Kevin Steinhardt <kevinsteinhardt@gmail.com>
2012-07-30 08:57:58 +0200

    Added script as is

---
+++ numbercruncher.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+
+# Checks that the name of a file in directory `{nietvrij,vrij}/12/1234` begins with `1234`
+# Checks that said file name also doesn’t contain spaces or “forbidden characters”
+# Checks that only PSDs are in the `vrij/` directory but anything can be in `nietvrij/`
+# Reports any discrepancies by printing messages to STDOUT
+
+
+exit;
