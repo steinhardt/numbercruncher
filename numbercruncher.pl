ea674e9aae10383a173e439e78fbc72a4d1fe048
Kevin Steinhardt <kevinsteinhardt@gmail.com>
2012-07-30 15:13:38 +0200

    Switched to File::Find for step 2

--- numbercruncher.pl
+++ numbercruncher.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/perl
 
+use File::Find;
+use warnings;
+
+
 my $scriptname = "watermarker.pl";
-@files = <*>; # Globs current directory
+my $dir = ".";
 
 
 # [1] Checks that the name of a file in directory `{nietvrij,vrij}/12/1234` begins with `1234`
@@ -9,12 +13,10 @@ print "FIXME - haven't coded this bit yet!\n";
 
 
 # [2] Checks that said file name also doesn’t contain spaces or "forbidden characters"
-foreach $file (@files) {
-	if ( $file =~ /^[A-Za-z0-9\.\-\+]+$/ ) {
-		print "";
-	}
-	else {
-		print "$scriptname - file $file contains forbidden characters. Only A-Z, a-z, 0-9, . (full stop), - (hyphen) and + (plus sign) allowed.\n";
+find(\&check_forbiddencharacters, $dir);
+sub check_forbiddencharacters {
+	if ( $_ !~ /^[A-Za-z0-9\.\-\+]+$/ ) {
+		print "$scriptname - file $_ contains forbidden characters. Only A-Z, a-z, 0-9, \. (full stop), - (hyphen) and + (plus sign) allowed.\n";
 	}
 }
 
